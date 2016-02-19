19c070a6a726aa45ae42f8ca884c63a24ce8c8ce
Hannasmith <a.shamowa2010@yandex.ru>
2016-02-19 21:10:44 +0800

    Доработка сайта

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//$(function(){
+//    $(".range-control").slider({
+//        range: true,
+//        min: 0,
+//        max: 20000,
+//        values: [0,15000],
+//        slide: function(event, ui ) {
+//            $(".price-control .min-price").val(ui.values[0]);
+//            $(".price-control .max-price").val(ui.values[1]);
+//        }
+//    });
